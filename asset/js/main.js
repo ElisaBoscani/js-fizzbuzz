b4939f43f15cb5f14f8c24ea1afb8e7cde7b9847
Elisa Boscani <elisabosc90@gmail.com>
2023-08-25 15:06:02 +0200

    initial commit

---
+++ asset/js/main.js
@@ -0,0 +1,6 @@
+//stampare numeri da 1 a 100 (ciclo for)
+//if/else per dividere in numeri:
+//-i numeri multipli di 3 devono essere stampati con una stringa
+//-i multipli di 5 devono essere stampati con una stinga
+//-i multipli sia di 3 che di 5 devono essere stampati tramite una stringa
+// va mandato a scermo
